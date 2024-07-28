60207dad636dee943c98f9eef068e9384bfe113e
Vipul <vipuldamrepro26@gmail.com>
2024-07-28 20:22:30 +0530

    make a server side rendering

--- 08_URL_Shortner_Project/routes/staticRouter.js
+++ 08_URL_Shortner_Project/routes/staticRouter.js
@@ -3,9 +3,11 @@ const URL = require('../models/url');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    const allUrls = URL.find({});
-    return res.render('home');
+router.get('/', async (req, res) => {
+    const allurls = await URL.find({});
+    return res.render('home', {
+        urls: allurls,
+    });
 })
 
 
