f9165d8240a5b70209b5cbcd379c8ec3165c992b
Vipul <vipuldamrepro26@gmail.com>
2024-08-26 18:03:31 +0530

    make a basic app now started making server and routes

--- 10_uploadFile/server.js
+++ 10_uploadFile/server.js
@@ -1,13 +1,21 @@
 const express = require('express');
+const path = require('path');
 const dbConnection = require('./connect');
 
 const PORT = 8008;
-
 const app = express();
 
+
 // database connections 
+
 dbConnection('')
 
+
+
+// set the view engine to ejs
+
+app.set('view engine', 'ejs');
+app.set('views', path.resolve('./views'));
 
 
 // Middlewares 
@@ -17,4 +25,10 @@ app.use(express.json());
 
 // Routes 
 
-a
+app.get('/', (req, res) => {
+    return res.render('homepage');
+});
+
+
+
+app.listen(PORT, () => console.log(`Server started at port: ${PORT}`))
